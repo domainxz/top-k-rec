89a51ff37ffcf28e0a1c9be8ccd30c408be658a4
dxz <domainxz@gmail.com>
2018-06-20 12:36:45 +1000

    Add wmf and cer, change the model saving method

--- train.py
+++ train.py
@@ -4,20 +4,20 @@ from single.vbpr import VBPR
 from single.wmf import WMF
 from single.cer import CER
 
-'''model = BPR(k=50);
+model = BPR(k=50);
 model.load_training_data('data/f0tr.txt', 'data/uid', 'data/vid');
 model.train('embed/bpr', epochs=5, batch_size=256);
 
 model = VBPR(k=50, d=20000);
 model.load_training_data('data/f0tr.txt', 'data/uid', 'data/vid');
 model.load_content_data('data/meta.pkl', 'data/vid');
-model.train('embed/vbpr', epochs=5, batch_size=256);'''
+model.train('embed/vbpr', epochs=5, batch_size=256);
 
-'''model = WMF(k=50);
+model = WMF(k=50);
 model.load_train_data('data/uid', 'data/vid', 'data/f0tr.txt');
-model.train('embed/wmf', max_iter=200, tol=1e-4);'''
+model.train('embed/wmf', max_iter=200, tol=1e-4);
 
 model = CER(k=50, d=20000);
 model.load_train_data('data/uid', 'data/vid', 'data/f0tr.txt');
 model.load_content_data('data/meta.pkl', 'data/vid');
-model.train('embed/cer', max_iter=3);
+model.train('embed/cer', max_iter=200);
