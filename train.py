5916a5d1015c85f82417d309b12464c7067f74bb
Xingzhong Du <domainxz@gmail.com>
2019-12-29 16:14:16 +1100

    Modify train.py to adapt to new interfaces

--- train.py
+++ train.py
@@ -17,15 +17,15 @@ model.load_content_data('data/meta.pkl', 'data/vid')
 model.train('embed/vbpr', epochs=5, batch_size=256)
 
 model = WMF(k=50)
-model.load_train_data('data/uid', 'data/vid', 'data/f0tr.txt')
+model.load_training_data('data/uid', 'data/vid', 'data/f0tr.txt')
 model.train('embed/wmf', max_iter=200, tol=1e-4)
 
 model = CER(k=50, d=20000)
-model.load_train_data('data/uid', 'data/vid', 'data/f0tr.txt')
+model.load_training_data('data/uid', 'data/vid', 'data/f0tr.txt')
 model.load_content_data('data/meta.pkl', 'data/vid')
 model.train('embed/cer', max_iter=200)
 
 model = DPM(k = 50, d = 20000)
-model.load_train_data('data/uid', 'data/vid', 'data/f0tr.txt')
+model.load_training_data('data/uid', 'data/vid', 'data/f0tr.txt')
 model.load_content_data('data/meta.pkl', 'data/vid')
 model.train('embed/dpm', MLP, max_iter=200)
